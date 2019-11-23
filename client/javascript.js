28e3c868ba6a7dd925ab7533472142c718d1769e
daan <d.van.turnhout013@gmail.com>
2019-11-23 15:18:27 +0100

    Add files via upload

--- client/javascript.js
+++ client/javascript.js
@@ -3,6 +3,8 @@ var sock = io();
 
 console.log("javascript is gelinkt");
 
+
+//login
 document.getElementById("login-form").addEventListener("submit", (e) => {
     //'preventDefault' => zorgt ervoor dat de pagina niet wordt herladen.
     e.preventDefault()
@@ -12,20 +14,29 @@ document.getElementById("login-form").addEventListener("submit", (e) => {
     document.getElementById("login-form-username").value = "";
 });
 
-sock.on("server-alert", (message) => {
-    alert(message);
-});
-
 sock.on("login-request-accepted", (username) => {
     document.getElementById("login-wrapper").style.display = "none";
     document.getElementById("main-wrapper").style.display = "block";
     document.getElementById("test").innerHTML += ` ${username}`;
 })
+//
+
+//debug
+sock.on("server-alert", (message) => {
+    alert(message);
+});
+//
+
+//lobby
+sock.on("lobby-status-update", (status) => {
+    document.getElementById("test2").innerHTML += ` ${status}`;
+});
 
 sock.on("lobby-queue-update", (Players) => {
-    document.getElementById("main-lobby-amount").innerHTML = `${Players.length}/10 spelers in de lobby:(?)`
+    document.getElementById("main-lobby-amount").innerHTML = `${Players.length}/6 spelers in de lobby`;
     document.getElementById("main-lobby-queue").innerHTML = "";
     Players.forEach((player) => {
         document.getElementById("main-lobby-queue").innerHTML += `${player.username}<br>`;
     });
 });
+//
