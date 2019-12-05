6ffced8f9ede1a4e8ed4defc48b6aea0cba67e2f
ArianVT <58031523+ArianVT@users.noreply.github.com>
2019-12-05 12:07:09 +0100

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,14 @@
+function Select_Chancelor() {
+	document.getElementById("Chancelor-dropdown").className = "verschijnen";
+}
+
+function Select_Chancelor_End() {
+	document.getElementById("Chancelor-dropdown").className = "verdwijnen";
+}
+
+function Select_Ja_Nein(){
+	document.getElementById("Ja/Nein-dropdown").className = "verschijnen"
+}
+function Select_Ja_Nein_End() {
+	document.getElementById("Ja/Nein-dropdown").className = "verdwijnen";
+}
