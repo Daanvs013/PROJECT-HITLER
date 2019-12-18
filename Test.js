11ccbc1d12bce102b09194df1ba5aeff60da03cf
ArianVT <58031523+ArianVT@users.noreply.github.com>
2019-12-18 10:59:10 +0100

    Add files via upload

--- Test.js
+++ Test.js
@@ -3,12 +3,25 @@ function Select_Chancelor() {
 }
 
 function Select_Chancelor_End() {
+	var chancelor_vote = document.getElementById("Chancelor-vote").value;
+	console.log(chancelor_vote);
 	document.getElementById("Chancelor-dropdown").className = "verdwijnen";
+	sock.emit('chancelor_vote', chancelor_vote);
 }
 
 function Select_Ja_Nein(){
-	document.getElementById("Ja/Nein-dropdown").className = "verschijnen"
+	document.getElementById("Ja/Nein-dropdown").className = "verschijnen";
 }
-function Select_Ja_Nein_End() {
+function Select_Ja_Nein_End(){
+	var ja_nein_vote = document.getElementById("ja_nein_vote").value;
+	console.log(ja_nein_vote);
 	document.getElementById("Ja/Nein-dropdown").className = "verdwijnen";
+	sock.emit('ja_nein_vote', ja_nein_vote);
+}
+
+
+function Show_Role(){
+	document.getElementById("Party_Role").className = "verschijnen";
+	document.getElementById("Secret_Role").className = "verschijnen";
+	setTimeout(function(){ document.getElementById("Secret_Role").className = "verdwijnen"; document.getElementById("Party_Role").className = "verdwijnen";}, 3000);
 }
