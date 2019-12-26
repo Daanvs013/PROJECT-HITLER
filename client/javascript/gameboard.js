7c88185852ad3b990f1baf200a2c0bbb9544c63b
daan <d.van.turnhout013@gmail.com>
2019-12-26 14:05:52 +0100

    Add files via upload

--- client/javascript/gameboard.js
+++ client/javascript/gameboard.js
@@ -106,8 +106,8 @@ document.getElementById("Chancellor-vote-form").addEventListener("submit", (e) =
 //functies waarmee de spelers kunnen stemmen op de gekozen kanselier
 sock.on("game-vote-chancellor", (chancellor) => {
     document.getElementById("Chancellor-dropdown").className = "verdwijnen";
-    document.getElementById("Ja/Nein-dropdown").className = "verschijnen";
-    document.getElementById("choice").innerHTML = `Wil je dat ${chancellor} kanselier word?`;
+    document.getElementById("Ja-Nein-dropdown").className = "verschijnen";
+    document.getElementById("Ja-Nein-dropdown-info").innerHTML = `Wil je dat ${chancellor} kanselier word?`;
 })
 
 document.getElementById("ja_nein_vote-form").addEventListener("submit", (e) => {
@@ -116,7 +116,7 @@ document.getElementById("ja_nein_vote-form").addEventListener("submit", (e) => {
     var choice = document.getElementById("ja_nein_vote").value;
     console.log(`Je hebt ${choice} gekozen`);
     sock.emit("game-chancellor-vote-choice", choice);
-    document.getElementById("Ja/Nein-dropdown").className = "verdwijnen";
+    document.getElementById("Ja-Nein-dropdown").className = "verdwijnen";
 });
 
 //functies om de getrokken kaarten te laten zien aan de president
@@ -125,6 +125,7 @@ sock.on("game-give-cards-president", (package) => {
     var element = document.getElementById("president-policies-popup");
     element.classList.remove("verdwijnen");
     element.classList.add("verschijnen");
+    element.innerHTML = 'Kies een beleidskaart die je weggooit.<br><br>';
     for (var i = 0; i < package.length; i++){
         element.innerHTML += `<div id="${i}" style="background-image: url(${package[i].path})" class="policies" onclick="ChoosePresidentPolicy(this.id)"></div>`
     }
@@ -144,6 +145,7 @@ sock.on("game-give-cards-chancellor", (package) => {
     var element = document.getElementById("chancellor-policies-popup");
     element.classList.remove("verdwijnen");
     element.classList.add("verschijnen");
+    element.innerHTML = 'Kies welk beleidskaart je wilt opleggen.<br><br>';
     for (var i = 0; i < package.length; i++){
         element.innerHTML += `<div id="${i}" style="background-image: url(${package[i].path})" class="policies" onclick="ChooseChancellorPolicy(this.id)"></div>`
     }
