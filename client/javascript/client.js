e8981fcf4f9fae8e54b6f408f2b9ad64cfef7652
Epic-NSA-Hacker <58031722+Epic-NSA-Hacker@users.noreply.github.com>
2020-01-09 12:08:26 +0100

    Add files via upload

---
+++ client/javascript/client.js
@@ -0,0 +1,35 @@
+//socket.io module.
+var sock = io();
+//
+
+//sessie
+var url = window.location.href;
+var data = sessionStorage.getItem('sessionID');
+console.log(`Sessie ID:${data}, Sessie path:${url}`);
+sock.emit("new-session", {ID:data, path:url});
+
+sock.on("sessionID", (ID) => {
+    //sla het ID op als sessieopslag
+    sessionStorage.setItem('sessionID', ID);
+});
+
+//als de client het tabblad of de browser sluit, wordt beforeunload nog uitgevoerd.
+window.addEventListener('beforeunload', (event) => {
+  sock.emit("beforeunload", {ID:data, path:url});
+});
+
+//test
+console.log("javascript is gelinkt");
+//
+
+//debug
+sock.on("server-alert", (message) => {
+    alert(message);
+});
+//
+
+//redirect functie
+sock.on("redirect-client", (path) => {
+    //console.log(path);
+    window.location.replace(path);
+});
