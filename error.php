064f3666d695404cef5c18593ca2f5af7426a101
cjcscheper <40724281+Kingcccas@users.noreply.github.com>
2020-01-09 12:06:47 +0100

    Update error.php

--- error.php
+++ error.php
@@ -14,8 +14,8 @@
 <?php 
 	function OpenCon() {
 		$dbhost = "localhost";
-		$dbuser = "zesvwo";
-		$dbpass = "Scheper4002";
+		$dbuser = "zesvwo1";
+		$dbpass = "Mercedes2016#";
 		$db = "zesvwo";
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 	return $conn;
